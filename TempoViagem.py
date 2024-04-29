2d528e5e69fcd076224b027d9381c1dee8193c88
Loise <loisefrangiosi22@gmail.com>
2024-04-29 15:12:00 -0300

    v0.1

---
+++ TempoViagem.py
@@ -0,0 +1,8 @@
+
+distancia = float(input("Digite a distância da viagem em quilômetros: "))
+
+velocidade = float(input("Digite a velocidade média da viagem em km/h: "))
+
+tempo = distancia / velocidade
+
+print("O tempo de viagem será de aproximadamente", tempo, "horas.")
